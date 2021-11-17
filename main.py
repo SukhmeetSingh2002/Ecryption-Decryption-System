c64cb2356c333e37056783b4104a8842ea3e7844
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2021-11-17 10:09:22 +0530

    Adds required changes

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import os
-import time
-
-inputFilePath="my_file.txt"
 
 def convert(x):
     if(ord(x)>=65 and ord(x)<=90):
@@ -28,12 +25,11 @@ def converted(x):
     else:
         z=" "
     return z
-
 def convertWordToBinary(word,lenWord=60):
     binWord=""
     for x in word:
         binWord=binWord+convert(x)
-    binWord="0"*(lenWord-len(binWord))+binWord
+    binWord=binWord+"0"*(lenWord-len(binWord))
     return binWord
 
 def splitData(dataString,lenWord=10):
@@ -44,89 +40,52 @@ def splitData(dataString,lenWord=10):
         dataString=dataString[lenWord:]
     return dataAsList
 
-def TakeFileInput(numOfOp):
+def TakeFileInput():
+    inputFilePath=input("Enter input file path: ")
     with open(inputFilePath, "r") as inputDataFile:
-        dataAsList = []
         dataAsString=inputDataFile.read()
-        dataAsList=splitData(dataAsString);
-        for x in range(len(dataAsList)):
-            if dataAsList[x]!='\n':
-                dataAsList[x]=convertWordToBinary(dataAsList[x])
-
+        dataAsString=dataAsString.replace("\n","____")
+        dataAsString=dataAsString.replace(".", "___")
+        dataAsString=dataAsString.replace(",", "____")
+        dataAsList=splitData(dataAsString)
+        dataAsList=[convertWordToBinary(x) for x in dataAsList]
     with open("my_file.txt", "w") as inputDataFile:
-        inputDataFile.write(str(numOfOp))
-        inputDataFile.write("\n")
-        inputDataFile.write("0")
+        inputDataFile.write(str(len(dataAsList)))
         inputDataFile.write("\n")
         for x in dataAsList:
-            inputDataFile.write(x)
-            if '\n' not in x:
-                inputDataFile.write('\n')
+            inputDataFile.write(f"0\n{x}\n")
 
-def TakeFileInputDec(numOfOp):
+def TakeFileInputDec():
+    inputFilePath=input("Enter path of file to be decrypted")
     with open(inputFilePath, "r") as inputDataFile:
         dataAsList = []
         dataAsString=inputDataFile.read()
+        dataAsString=dataAsString.replace("\n","")
+        dataAsString=dataAsString.replace(".", "")
+        dataAsString=dataAsString.replace(",", "")
         dataAsList=splitData(dataAsString,12);
-        for x in range(len(dataAsList)):
-            if dataAsList[x]!='\n':
-                dataAsList[x]=convertWordToBinary(dataAsList[x],78)
-
+        dataAsList=[convertWordToBinary(x,78) for x in dataAsList]
     with open("my_file.txt", "w") as inputDataFile:
-        inputDataFile.write(str(numOfOp))
-        inputDataFile.write("\n")
-        inputDataFile.write("1")
+        inputDataFile.write(str(len(dataAsList)))
         inputDataFile.write("\n")
         for x in dataAsList:
-            inputDataFile.write(x)
-            if '\n' not in x:
-                inputDataFile.write('\n')
+            inputDataFile.write(f"1\n{x}\n")
 
-def OutputFile():
-    with open(inputFilePath, "r") as f:
+def OutputFile(TypeOfOp):
+    with open("out_file.txt", "r") as f:
         dataAsBinary=f.readlines()
-        c=[]
-        TypeOfOp=int(dataAsBinary[1])
-        dataAsBinary=dataAsBinary[1:]
-        dataAsBinary=dataAsBinary[1:]
         if TypeOfOp==0:
             for i in dataAsBinary:
                 if i!='\n':
                     for j in range(10):
-                        c=c+list(converted(int(i[6*j:6*j+6],2)))
+                        c=c.append(list(converted(int(i[6*j:6*j+6],2))))
                 else:
                     c=c+list("\n")
-            newc=[]
-            for x in range(len(c)):
-                if x>0 and c[x]==' ' and c[x-1]==' ':
-                    pass
-                else:
-                    newc.append(c[x])
-
             with open("new.txt", "w") as fout:
-                fout.write("output\n")
-                for x in range(len(newc)):
-                    fout.write(newc[x])
+                fout.writelines(c)
 
         elif TypeOfOp==1:
-            for i in dataAsBinary:
-                if i!='\n':
-                    for j in range(12):
-                        c=c+list(converted(int(i[6*j:6*j+6],2)))
-                else:
-                    c=c+list("\n")
-            newc=[]
-            for x in range(len(c)):
-                if x>0 and c[x]==' ' and c[x-1]==' ':
-                    pass
-                else:
-                    newc.append(c[x])
-
-            with open("new.txt", "w") as fout:
-                fout.write("output\n")
-                for x in range(len(newc)):
-                    fout.write(newc[x])
-        elif TypeOfOp==2:
+            c=[]
             for i in dataAsBinary:
                 if i!='\n':
                     for j in range(10):
@@ -139,50 +98,75 @@ def OutputFile():
                     pass
                 else:
                     newc.append(c[x])
-
             with open("new.txt", "w") as fout:
                 fout.write("output\n")
                 for x in range(len(newc)):
                     fout.write(newc[x])
-
-
+            ans=""
+            with open("new.txt", "r") as fout:
+                ans=fout.read()
+            ans.replace("____","\n")
+            ans.replace("___",".")
+            ans.replace("____",",")
+            with open("new.txt", "w") as fout:
+                fout.write(ans)
 
 x=int(input("Press 1 if you know number of operations to be done else press 2 if you dont know number of operations"))
 if(x==1):
-    TypeOfOp=int(input("Press 1 to encrypt file\n2 to decrypt file\n3 to generate password"))
-    numberOfOp=int(input("Enter number of operations: "))
-    if TypeOfOp==1:
-        print("Enter the path of file to be enctrypted : ")
-        TakeFileInput(numberOfOp)
-    elif TypeOfOp==2:
-        print("Enter the path of file to be dectrypted : ")
-        TakeFileInputDec(numberOfOp)
-    else:
-        with open("my_file.txt", "w") as inputDataFile:
-            inputDataFile.write(str(numberOfOp))
-            inputDataFile.write("\n")
-            inputDataFile.write("2")
-            inputDataFile.write("\n")
+    try:
+        q=int(input("Enter number of operations: "))
+    except:
+        q=-1
+    if(q!=-1):
+        with open("my_file.txt", "w") as file:
+            file.write(q)
+            file.write("\n")
+            for i in range(q):
+                t=input("Enter {}th password max characters = 10: ".format(i))
+                r=""
+                for x in t:
+                    r=r+convert(x)
+                r="0"*(60-len(r))+r
+                file.write(r)
+                file.write("\n")
 elif(x==2):
-    while(TypeOfOp<4):
-        TypeOfOp=input("Press 1 to encrypt file\n2 to decrypt file\n3 to generate password\n4 to exit")
-        if TypeOfOp==1:
-            print("Enter the path of file to be enctrypted : ")
-            TakeFileInput(1)
-        elif TypeOfOp==2:
-            print("Enter the path of file to be dectrypted : ")
-            TakeFileInputDec(1)
-        else:
-            with open("my_file.txt", "w") as inputDataFile:
-                inputDataFile.write(1)
-                inputDataFile.write("\n")
-                inputDataFile.write("2")
-                inputDataFile.write("\n")
+    print("Enter any character to stop taking input")
 else:
     print("Invalid Input")
 
 os.popen("vvp.exe a.out")
-
-time.sleep(2)
-
-OutputFile()
+#OUTPUT
+with open("out_file.txt", "r") as file:
+    with open("my_file.txt", "r") as y:
+        n=int(y.readline())
+        n=int(y.readline())
+        print(n)
+        for i in range(n):
+            w=int(y.readline())
+            print(w)
+            if w == 0:
+                u=y.readline()
+                i=file.readline()
+                c=""
+                k=""
+                for j in range(12):
+                    c=c+converted(int(i[6*j:6*j+6],2))
+                for j in range(10):
+                    k=k+converted(int(u[6*j:6*j+6],2))
+                print(f"initial password {k} encrypted password {c}")
+            elif w == 1:
+                u=y.readline()
+                i=file.readline()
+                c=""
+                k=""
+                for j in range(10):
+                    c=c+converted(int(i[6*j:6*j+6],2))
+                for j in range(12):
+                    k=k+converted(int(u[6*j:6*j+6],2))
+                print(f"given password is {k} and its decrypted password is {c}")
+            elif w == 2:
+                i=file.readline()
+                c=""
+                for j in range(10):
+                    c=c+converted(int(i[6*j:6*j+6],2))
+                print(f"passowrd generated is : {c}")
